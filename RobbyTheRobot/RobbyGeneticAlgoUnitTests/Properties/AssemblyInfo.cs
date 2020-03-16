363dd179e22dc4dc6605ac69d410d066fd88489b
korjon <korjon0000@gmail.com>
2020-03-15 22:27:49 -0400

    Started the Chromosome class. Made the 2 constructors, private member array, and few classes. Made allele enum

---
+++ RobbyTheRobot/RobbyGeneticAlgoUnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("RobbyGeneticAlgoUnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RobbyGeneticAlgoUnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("fa87d4c7-ca30-4f54-92db-95bfa4ee0eba")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
