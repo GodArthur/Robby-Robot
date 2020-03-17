6bc584977c4418610848e5d6bc338bb4dd98f621
korjon <korjon0000@gmail.com>
2020-03-16 22:41:52 -0400

    created crossover methods in Chromosome class

--- RobbyTheRobot/RobbyGeneticAlgo/Chromosome.cs
+++ RobbyTheRobot/RobbyGeneticAlgo/Chromosome.cs
@@ -21,7 +21,8 @@ namespace RobbyGeneticAlgo
             get; private set;
         }
         
-
+        public int Length
+        { get; }
 
         /// <summary>
         /// Chromosome constructor initializing 
@@ -30,6 +31,8 @@ namespace RobbyGeneticAlgo
         /// <param name="Length">Initial length of the allele</param>
         public Chromosome(int Length)
         {
+            this.Length = Length;
+
             //setting the length of the alleles
             gene = new Allele[Length];
 
@@ -79,6 +82,11 @@ namespace RobbyGeneticAlgo
             Fitness = f(this);
         }
 
+        /// <summary>
+        /// Method converts array of allele to string.
+        /// Overrides ToString method in object class
+        /// </summary>
+        /// <returns>newly converted string of allele</returns>
         public override String ToString()
         {
             string content = "";
@@ -96,6 +104,111 @@ namespace RobbyGeneticAlgo
             }
 
             return content;
+        }
+
+        public int CompareTo(Chromosome c)
+        {
+            return (int)(this.Fitness - c.Fitness);
+        }
+
+        public Chromosome[] SingleCrossover(Chromosome a, Chromosome b)
+        {
+            //declaring the new subset of offspring allele
+            Allele[] firstSet = new Allele[a.Length];
+            Allele[] secondSet = new Allele[a.Length];
+
+            //number at which splits the two chromosomes
+            int split = Helpers.rand.Next(0, a.Length - 1);
+
+            //setting first set of allele
+            for (int i = 0; i < firstSet.Length; i++)
+            {
+                if (i <= split)
+                {
+                    firstSet[i] = a[i];
+                }
+                else
+                {
+                    firstSet[i] = b[i];
+                }
+            }
+
+            //setting the second set of allele
+            for (int i = 0; i < secondSet.Length; i++)
+            {
+                if (i <= split)
+                {
+                    secondSet[i] = b[i];
+                }
+                else
+                {
+                    secondSet[i] = a[i];
+                }
+            }
+
+            Chromosome[] children = new Chromosome[2];
+
+            //storing the newly created chromosomes
+            children[0] = new Chromosome(firstSet);
+            children[1] = new Chromosome(secondSet);
+
+            return children;
+        }
+
+        public Chromosome[] DoubleCrossover(Chromosome a, Chromosome b)
+        {
+            //declaring the new subset of offspring allele
+            Allele[] firstSet = new Allele[a.Length];
+            Allele[] secondSet = new Allele[a.Length];
+
+            //choosing the first and second halfway random points
+            int half1 = Helpers.rand.Next(0, (a.Length / 2) - 1);
+            int half2 = Helpers.rand.Next(a.Length / 2, a.Length - 1);
+
+
+            //storing the first set of allele
+            for (int i = 0; i < firstSet.Length; i++)
+            {
+                if (i <= half1)
+                {
+                    firstSet[i] = a[i];
+                }
+                else if (i > half1 && i <= half2)
+                {
+                    firstSet[i] = b[i];
+                }
+
+                else 
+                {
+                    firstSet[i] = a[i];
+                }
+            }
+
+
+            //storing the second set of allele
+            for (int i = 0; i < secondSet.Length; i++)
+            {
+                if (i <= half1)
+                {
+                    secondSet[i] = b[i];
+                }
+                else if (i > half1 && i <= half2)
+                {
+                    secondSet[i] = a[i];
+                }
+
+                else
+                {
+                    secondSet[i] = b[i];
+                }
+            }
+
+            Chromosome[] children = new Chromosome[2];
+
+            children[0] = new Chromosome(firstSet);
+            children[1] = new Chromosome(secondSet);
+
+            return children;
         }
     }
 }
